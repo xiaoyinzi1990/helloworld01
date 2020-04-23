5518102412bc86d450a46dd2aa54bd32bb2c6473
xiaoyinzi1990 <1342032527@qq.com>
2020-04-23 10:24:59 +0800

    修改test

--- src/com/yinzi/Test.java
+++ src/com/yinzi/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Test.main");
         System.out.println("Test.main2");
+        System.out.println("Test.main3");
     }
 }
